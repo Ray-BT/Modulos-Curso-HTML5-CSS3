57aba6df67454e3f4fc2b705517aaae28efba972
Ray Benvinda Trajano <ray.benvinda@gmail.com>
2023-03-28 14:14:03 -0300

    menu responsivo com media query + JS

--- mod3/ex026/mq005/script.js
+++ mod3/ex026/mq005/script.js
@@ -5,3 +5,10 @@ function clickMenu() {
         itens.style.display = 'block'
    }
 }
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
